8aa2aef792261d792b6858a27d4810629ab6cd46
Tom Igoe <tom.igoe@gmail.com>
2021-04-12 15:17:26 -0400

    update p5JsClient

--- p5jsClient/sketch.js
+++ p5jsClient/sketch.js
@@ -6,7 +6,7 @@
 
 */
 
-var socket = new WebSocket("ws://localhost:8081");
+var socket = new WebSocket("ws://localhost:8080");
 
 function setup() {
 	// The socket connection needs two event listeners:
