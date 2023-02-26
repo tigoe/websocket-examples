a7affba62bebfefbd8c42c5d2ee6628e3c440472
Tom Igoe <tom.igoe@gmail.com>
2023-02-26 13:58:43 -0500

    updated expressWsServer

--- ExpressWsServer/server.js
+++ ExpressWsServer/server.js
@@ -9,32 +9,35 @@
   modified 23 Feb 2023
   by Tom Igoe
 */
-
-var express = require('express');			    // include express.js
-// a local instance of express:
-var server = express();
-// instance of the websocket server:
-var WebSocketServer = require('ws').Server;   // webSocket library
-// configure the webSocket server:
-const wssPort = process.env.PORT || 8080;             // port number for the webSocket server
-const wss = new WebSocketServer({port: wssPort}); // the webSocket server
-
+// include express, http, and ws libraries:
+const express = require("express");
+const {createServer} = require("http");
+const {WebSocketServer} = require("ws");
+// make an instance of express:
+const app = express();
+// serve static content from the project's public folder:
+app.use(express.static("public"));
+// make an instance of http server using express instance:
+const server = createServer(app);
+// WebSocketServer needs the http server instance:
+const wss = new WebSocketServer({ server });
 // list of client connections:
-var clients = new Array;
+var clients = new Array();
 
-// serve static files from /public:
-server.use('/', express.static('public'));
-
-// this runs after the server successfully starts:
+// this runs after the http server successfully starts:
 function serverStart() {
   var port = this.address().port;
-  console.log('Server listening on port ' + port);
+  console.log("Server listening on port " + port);
 }
 
+// this handles websocket connections:
 function handleClient(thisClient, request) {
-  console.log("New Connection");        // you have a new client
-  clients.push(thisClient);    // add this client to the clients array
+  // you have a new client
+  console.log("New Connection"); 
+  // add this client to the clients array
 
+  clients.push(thisClient); 
+  
   function endClient() {
     // when a client closes its connection
     // get the client's position in the array
@@ -46,24 +49,24 @@ function handleClient(thisClient, request) {
 
   // if a client sends a message, print it out:
   function clientResponse(data) {
-    console.log(request.connection.remoteAddress + ': ' + data);
-		broadcast(request.connection.remoteAddress + ': ' + data);
+    console.log(data.toString());
+    broadcast(data.toString());
+  }
+
+  // This function broadcasts messages to all webSocket clients
+  function broadcast(data) {
+    // iterate over the array of clients & send data to each
+    for (let c in clients) {
+      clients[c].send(data);
+    }
   }
 
   // set up client event listeners:
-  thisClient.on('message', clientResponse);
-  thisClient.on('close', endClient);
-}
-
-// This function broadcasts messages to all webSocket clients
-function broadcast(data) {
-  // iterate over the array of clients & send data to each
-  for (let c in clients) {
-    clients[c].send(data);
-  }
+  thisClient.on("message", clientResponse);
+  thisClient.on("close", endClient);
 }
 
 // start the server:
 server.listen(process.env.PORT || 3000, serverStart);
-// listen for websocket clients and handle them:
-wss.on('connection', handleClient);
+// start the websocket server listening for clients:
+wss.on("connection", handleClient);
